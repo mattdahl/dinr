4cc85f95f84179b528fc361cd782f40609f0a397
Jerry Yan <jerry.yan96@gmail.com>
2015-04-11 06:42:34 -0700

    Fixed parse interface with pulling matches

--- Dinr/src/edu/pomona/dinr/ParseInterface.java
+++ Dinr/src/edu/pomona/dinr/ParseInterface.java
@@ -1,6 +1,12 @@
 package edu.pomona.dinr;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
+
+
 
 
 
@@ -13,17 +19,17 @@ import org.json.*;
 import org.json.simple.parser.*;
 public class ParseInterface {
 
-	
+
 	public static final String DINNER = "dinner";
 	public static final String LUNCH = "lunch";
 	public static final String BREAKFAST = "breakfast";
 	public static final String BRUNCH = "brunch";
-	
+
 	private static String capitalize(String input) {
 		return Character.toUpperCase(input.charAt(0)) + input.substring(1);
 	}
-	
-	
+
+
 	/**
 	 * 
 	 * @param id
@@ -53,7 +59,7 @@ public class ParseInterface {
 		newStudent.put("pic_url", pic_url);
 		newStudent.saveInBackground();
 	}
-	
+
 	/**
 	 * 
 	 * @param id
@@ -64,19 +70,19 @@ public class ParseInterface {
 	public static void changeStudent(String id, final String[] breakfast, final String[] lunch, final String[] dinner) {
 		ParseQuery<ParseObject> query = ParseQuery.getQuery("Student");
 		query.getInBackground(id, new GetCallback<ParseObject>() {
-		  public void done(ParseObject object, ParseException e) {
-		    if (e == null) {
-		    	object.put("BreakfastHalls", breakfast);
-		    	object.put("LunchHalls", lunch);
-		    	object.put("DinnerHalls", dinner);
-		    	object.saveInBackground();
-		    } else {
-		      // something went wrong
-		    }
-		  }
+			public void done(ParseObject object, ParseException e) {
+				if (e == null) {
+					object.put("BreakfastHalls", breakfast);
+					object.put("LunchHalls", lunch);
+					object.put("DinnerHalls", dinner);
+					object.saveInBackground();
+				} else {
+					// something went wrong
+				}
+			}
 		});
 	}
-	
+
 	/**
 	 * 
 	 * 
@@ -87,44 +93,40 @@ public class ParseInterface {
 	private static Student[] students;
 	public static Student[] getMatches(String meal, String[] breakfastChoices) {
 		final JSONParser parser = new JSONParser();
-		
+		Map<String, List<String>> map = new HashMap<String, List<String>>();
+		map.put("diningHalls", Arrays.asList(breakfastChoices));
 		try {
-			JSONObject object = new JSONObject();
-			object.put(capitalize(meal).concat("Halls"), breakfastChoices);
-		    ParseCloud.callFunctionInBackground(meal, null, new FunctionCallback<String>() {
-		        public void done(String object, ParseException e) {
-		          if (e == null){   
-		            try {
-						JSONObject obj = (JSONObject) parser.parse(object);
-						JSONArray array = (JSONArray) obj.getJSONArray("result");
-						 students = new Student[array.length()];
-						for (int i = 0; i < array.length(); i++) {
-							JSONObject elt = array.getJSONObject(i);
-							students[i] = new Student(elt.getString("objectId"), elt.getString("name"), elt.getString("major"),
-									(String[]) elt.get("BreakfastHalls"), (String[]) elt.get("LunchHalls"), (String[]) elt.get("DinnerHalls"),
-									elt.getString("college"), elt.getString("year"), elt.getString("interests"), elt.getString("pic_url"));
-									
-						}
-						
-					} catch (JSONException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					} catch (org.json.simple.parser.ParseException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					} 
-					
-		          } 
-		        }
-		    });
-			
-			
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
+
+
+			ArrayList<ParseObject> object = ParseCloud.callFunction(meal, map);
+
+			System.out.println("Continuing " + object.size());
+			for (ParseObject obj: object) {
+				System.out.println("!!! => " + obj.toString());
+			}
+			int idx = 0;
+			students = new Student[object.size()];
+			for (ParseObject elt : object) {
+				
+				ArrayList<String> bA = ((ArrayList<String>) elt.get("BreakfastHalls"));
+				ArrayList<String> bL = ((ArrayList<String>) elt.get("LunchHalls"));
+				ArrayList<String> bD = ((ArrayList<String>) elt.get("DinnerHalls"));
+				String[] b = (bA == null) ? new String[0] : new String[bA.size()];
+				String[] l = (bL == null) ? new String[0] :new String[bL.size()];
+				String[] d = (bD == null) ? new String[0] :new String[bD.size()];
+				students[idx] = new Student(elt.getString("objectId"), elt.getString("name"), elt.getString("major"),
+						(bA == null) ? new String[0] : bA.toArray(b), (bL == null) ? new String[0] :bL.toArray(l), (bD == null) ? new String[0] :bD.toArray(d),
+						elt.getString("college"), elt.getString("year"), elt.getString("interests"), elt.getString("pic_url"));
+				idx = idx + 1;
+
+			}
+
+		} catch (Exception e) {
+			System.out.println("ERROR");
 			e.printStackTrace();
-			return new Student[0];
 		}
+		System.out.println("returning");
 		return (students == null) ? new Student[0] : students;
 	}
-	
+
 }
